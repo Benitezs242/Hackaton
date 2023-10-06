4db266ffbd93b5fd7255e61e7190e4e7dc1a19d4
Benitezs242 <benitezs242@gmail.com>
2023-10-05 19:28:37 -0500

    punto1

---
+++ js/ejercicio1.js
@@ -0,0 +1,7 @@
+// let BtnForm = document.querySelectorAll('#Fenviar')
+FCadena.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let ValorInput = e.target.iCadena.value
+    console.log(ValorInput)
+})
+
